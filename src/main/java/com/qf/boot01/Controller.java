a5b22283e3b37c9b7cd22df61515bcece50c157c
dtg <wsztg008@gmail.com>
2022-11-14 10:57:36 +1300

    1

---
+++ src/main/java/com/qf/boot01/Controller.java
@@ -0,0 +1,18 @@
+package com.qf.boot01;
+
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/receive")
+public class Controller {
+
+    @PostMapping("/status")
+    public String status (){
+
+        System.out.println("got msg");
+        return "SUCCESS";
+    }
+}
---
+++ src/main/java/com/qf/boot01/Test01.java
@@ -0,0 +1,15 @@
+package com.qf.boot01;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class Test01 {
+
+}
