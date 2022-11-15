c9ffdb4ab7625782b3b166b2a70eff77b73d1024
dtg <wsztg008@gmail.com>
2022-11-15 17:14:32 +1300

    docker

--- src/main/java/com/qf/boot01/Controller.java
+++ src/main/java/com/qf/boot01/Controller.java
@@ -13,6 +13,6 @@ public class Controller {
     public String status (){
 
         System.out.println("got msg");
-        return "SUCCESS from github";
+        return "SUCCESS from github   hahahahah";
     }
 }
